a6cb3db36c7cebbe1787668c4a1156f904a27626
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-06-06 11:02:36 -0500

    agregando bot de caarloshugo

--- caarloshugo.php
+++ caarloshugo.php
@@ -2,10 +2,9 @@
 <?php
 require_once("twitter/functions.php");
 require_once("twitter/twitter.php");
-require_once("config/codejobs.php");
+require_once("config/caarloshugo.php");
 
 foreach($keys as $key) {
 	print_r("\n\nUser: " . $key["username"]  . "-" . $key["userRT"] . "\n");
-	RT($key);
-	sleep(300);
+	RT_E($key);
 }
